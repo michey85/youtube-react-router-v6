364f14188d1ad9cd993ae7e54d735e0c06c14dcb
Mikhail Nepomnyashchiy <nem@kodix.ru>
2021-11-05 21:56:53 +0300

    basic example

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
+import { Homepage } from './pages/Homepage';
+import { About } from './pages/Aboutpage';
+import { Blogpage } from './pages/Blogpage';
+import { Notfoundpage } from './pages/Notfoundpage';
+
 function App() {
   return (
     <>
       <header>
-        <a href="/">Home</a>
-        <a href="/posts">Blog</a>
-        <a href="/about">About</a>
+        <Link to="/">Home</Link>
+        <Link to="/posts">Blog</Link>
+        <Link to="/about">About</Link>
       </header>
-      <div>
-            <h1>Get started with React-Router 6</h1>
-        </div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Blogpage />} />
+        <Route path="*" element={<Notfoundpage />} />
+      </Routes>
     </>
   );
 }
--- src/pages/Notfoundpage.jsx
+++ src/pages/Notfoundpage.jsx
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom'
+
 const Notfoundpage = () => {
     return (
         <div>
-            This page doesn't exist. Go <a href="/">home</a>
+            This page doesn't exist. Go <Link to="/">home</Link>
         </div>
     )
 }
